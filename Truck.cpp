7b8c31c05efb146a70c52cdeda2ee841026e19cc
Tdarrelpa <darrel2208@gmail.com>
2024-12-18 21:52:36 +0700

    Tinggal mikirin cara sambungin 'Truck.h' dan 'rute.h' biar nggak konflik pas dijalanin di 'main.cpp'nya, sama sekalian beresin 'main.cpp'nya, baru beres

--- Truck.cpp
+++ Truck.cpp
@@ -1,19 +1,4 @@
 #include "Truck.h"
-<<<<<<< HEAD
-#include <iostream>
-
-using namespace std;
-
-void CreateTruck(TruckList &T) {
-    first(T) = NULL;
-}
-
-adr_Truck Alokasi(Truck x) {
-    adr_Truck P = new elm_Truck; 
-    if (P != NULL) {
-        info(P) = x;     
-        next(P) = NULL; 
-=======
 
 void CreateTruck(TruckList &T)
 {
@@ -27,63 +12,34 @@ adr_Truck Alokasi(Truck x)
     {
         info(P) = x;
         next(P) = NULL;
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
     }
     return P;
 }
 
-<<<<<<< HEAD
-void addTruck(TruckList &T, Truck x) {
-    adr_Truck P = Alokasi(x);
-    if (P != NULL) {
-        next(P) = T.first; 
-=======
 void addTruck(TruckList &T, Truck x)
 {
     adr_Truck P = Alokasi(x);
     if (P != NULL)
     {
         next(P) = first(T);
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
         first(T) = P;
     }
 }
 
-<<<<<<< HEAD
-void AddMuatan(TruckList &T, string name, int muatan) {
-    adr_Truck P = T.first;
-    while (P != NULL) {
-        if (info(P).name == name) {
-=======
 void AddMuatan(TruckList &T, string name, int muatan)
 {
     for(adr_Truck P = first(T); P != NULL; P = next(P))
     {
         if (info(P).name == name)
         {
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
             info(P).muatan += muatan;
             cout << "Muatan " << muatan << " telah ditambahkan pada truck " << name << "." << endl;
             return;
         }
-<<<<<<< HEAD
-        P = next(P);
-=======
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
     }
     cout << "Truck dengan nama " << name << " tidak ditemukan!" << endl;
 }
 
-<<<<<<< HEAD
-void deleteMuatan(TruckList &T, string name, int muatan) {
-    adr_Truck P = T.first;
-    while (P != NULL) {
-        if (info(P).name == name) {
-            if (info(P).muatan >= muatan) {
-                info(P).muatan -= muatan;
-                cout << "Muatan " << muatan << " telah dihapus dari truck " << name << "." << endl;
-            } else {
-=======
 void deleteMuatan(TruckList &T, string name, int muatan)
 {
     for(adr_Truck P = first(T); P != NULL; P = next(P))
@@ -97,26 +53,14 @@ void deleteMuatan(TruckList &T, string name, int muatan)
             }
             else
             {
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
                 cout << "Muatan tidak cukup untuk dihapus!" << endl;
             }
             return;
         }
-<<<<<<< HEAD
-        P = next(P);
-=======
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
     }
     cout << "Truck dengan nama " << name << " tidak ditemukan!" << endl;
 }
 
-<<<<<<< HEAD
-void deleteTruck(TruckList &T, string name) {
-    adr_Truck P = first(T), prev = NULL;
-    
-    if (P != NULL && P->info.name == name) {
-        first(T) = next(P); 
-=======
 void deleteTruck(TruckList &T, string name)
 {
     adr_Truck P = first(T), prev = NULL;
@@ -124,28 +68,19 @@ void deleteTruck(TruckList &T, string name)
     if (P != NULL && P->info.name == name)
     {
         first(T) = next(P);
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
         delete P;
         cout << "Truck dengan nama " << name << " telah dihapus." << endl;
         return;
     }
 
-<<<<<<< HEAD
-    while (P != NULL && info(P).name != name) {
-=======
     while (P != NULL && info(P).name != name)
     {
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
         prev = P;
         P = next(P);
     }
 
-<<<<<<< HEAD
-    if (P == NULL) {
-=======
     if (P == NULL)
     {
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
         cout << "Truck dengan nama " << name << " tidak ditemukan!" << endl;
         return;
     }
@@ -155,20 +90,6 @@ void deleteTruck(TruckList &T, string name)
     cout << "Truck dengan nama " << name << " telah dihapus." << endl;
 }
 
-<<<<<<< HEAD
-double biayaPengiriman(TruckList &T, string name, double biayaPerMuatan, double biayaPerKm, double jarak) {
-    adr_Truck P = first(T);
-    while (P != NULL) {
-        if (info(P).name == name) {
-            double totalBiaya = (info(P).muatan * biayaPerMuatan) + (jarak * biayaPerKm);
-            return totalBiaya;
-        }
-        P = next(P);
-    }
-    cout << "Truck dengan nama " << name << " tidak ditemukan!" << endl;
-}
-
-=======
 double biayaPengiriman(TruckList &T, string name, double biayaPerMuatan, double biayaPerKm, double jarak)
 {
     for(adr_Truck P = first(T); P != NULL; P = next(P))
@@ -290,4 +211,3 @@ void PilihRute(TruckList &T, RoadList &R, string gudang, string jalur, string tr
     CatatJalur(R, gudang, jalur, truk);
     cout << "Rute yang dipilih: " << jalur << " dengan bensin yang diperlukan: " << bensinDiperlukan << " liter." << endl;
 }
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
--- Truck.h
+++ Truck.h
@@ -1,46 +1,24 @@
 #ifndef TRUCK_H_INCLUDED
 #define TRUCK_H_INCLUDED
 
-<<<<<<< HEAD
-#include <iostream>
-=======
 //Sementara kayak gini dulu, ini belum disambungin ke Rute.h soalnya
 #include <iostream>
 #include <string>
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
 using namespace std;
 
 #define first(L) (L).first
 #define next(P) (P)->next
 #define info(P) (P)->info
 
-<<<<<<< HEAD
-typedef struct elm_Truck* adr_Truck;
-
-struct Truck {
-=======
 typedef struct t_elm_Truck* adr_Truck;
 typedef struct t_elm_Road* adr_Road;
 
 typedef struct {
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
     string name;
     int jumlahRoda;
     double kapasitas;
     double bensin;
     int muatan;
-<<<<<<< HEAD
-};
-
-struct elm_Truck {
-    Truck info;
-    adr_Truck next;
-};
-
-struct TruckList {
-    adr_Truck first;
-};
-=======
 } Truck;
 
 typedef struct t_elm_Truck {
@@ -68,19 +46,12 @@ typedef struct
 {
     adr_Road first;
 } RoadList;
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
 
 void CreateTruck(TruckList &T);
 adr_Truck Alokasi(Truck x);
 void addTruck(TruckList &T, Truck x);
 void AddMuatan(TruckList &T, string name, int muatan);
 void deleteMuatan(TruckList &T, string name, int muatan);
-<<<<<<< HEAD
-void deleteTruck(TruckList &T, string x);
-double biayaPengiriman(TruckList L, string name, double biayaPerMuatan, double biayaPerKm, double jarak);
-
-#endif // TRUCK_H_INCLUDED
-=======
 void deleteTruck(TruckList &T, string name);
 double biayaPengiriman(TruckList &T, string name, double biayaPerMuatan, double biayaPerKm, double jarak);
 double KapasitasMaksimal(TruckList &T, string name);
@@ -90,4 +61,3 @@ void CatatJalur(TruckList &T, string gudang, string jalur, string truk);
 string JalurAlternatif(RoadList &R, string gudang, string jalur);
 void PilihRute(TruckList &T, RoadList &R, string gudang, string jalur, string truk, double bensin, double muatan, double jarak);
 #endif // TRUK_H_INCLUDED
->>>>>>> 4bb2cd9 (Tinggal mikirin cara sambungin 'Truck.h' dengan 'rute.h' doang sama benerin main program, baru beres)
